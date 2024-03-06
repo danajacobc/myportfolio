b12cc3c8c13eebb3bcf6a744fc4d4d40e9b6e11e
danajacobc <danajacobc@gmail.com>
2024-03-06 12:41:33 -0300

    tailwind config

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
 
 function App() {
-
   return (
-    <div>
-      Hello World
+    <div className='h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500'>
+      <h1 className='text-center text-5xl mb-4 font-display text-white'>Hello World - Dana Jacob Casanoves</h1>
+      <h1 className='text-center text-5xl mb-4 font-roboto text-white'>Hello World - Dana Jacob Casanoves</h1>
+
     </div>
   )
 }
---
+++ my-portfolio/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          display: ["Lato", ...defaultTheme.fontFamily.sans],
+          roboto: ["Roboto Slab", ...defaultTheme.fontFamily.sans]
+        }
+      },
+    },
+    plugins: [],
+  }
