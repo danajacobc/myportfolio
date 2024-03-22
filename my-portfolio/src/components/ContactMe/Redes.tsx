0ef6614868e38d7597fb47767396e398cd8ef46a
danajacobc <danajacobc@gmail.com>
2024-03-21 22:20:25 -0300

    contact me

---
+++ my-portfolio/src/components/ContactMe/Redes.tsx
@@ -0,0 +1,13 @@
+import logo from '../../assets/logo/principal.png'
+
+const Redes = () => {
+  return (
+    <div className='flex flex-col justify-center items-center'>
+        Redes
+        <h3>¡Trabajemos juntos!</h3>
+        <img src={logo} className="h-48 w-48"/>
+    </div>
+  )
+}
+
+export default Redes
--- my-portfolio/src/views/ContactMe/ContactMe.tsx
+++ my-portfolio/src/views/ContactMe/ContactMe.tsx
@@ -1,21 +1,21 @@
-import FormContact from "../../components/ContactMe/FormContact";
-import logo from '../../assets/logo/principal.png'
+// import FormContact from "../../components/ContactMe/FormContact";
+import Redes from "../../components/ContactMe/Redes";
 
 const ContactMe = () => {
   return (
-    <div className="mt-2">
-        <div className="flex flex-col justify-center items-center font-bold text-2xl">
+    <div className="flex flex-col justify-center items-center">
+        <div className="font-bold text-2xl">
            <h1 className='bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] text-transparent bg-clip-text'>Contacto</h1>
         </div>
-        <div className="flex flex-row justify-center items-center gap-8">
-          <div className="w-1/2 flex flex-col justify-center items-center gap-8">
-            <h3>¡Trabajemos juntos!</h3>
-            <img src={logo} className="h-48 w-48"/>
-          </div>
-          <div className="w-1/2">
-            <FormContact />
-          </div>
+       
+        <div className="flex flex-col justify-center items-center gap-8">
+          <Redes /> 
         </div>
+
+          {/* <div className="w-1/2">
+            <FormContact />
+          </div> */}
+        
     </div>
   )
 }
