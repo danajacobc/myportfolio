eff6d2108a1021e8b5c0ec465f4c0bae18291deb
danajacobc <danajacobc@gmail.com>
2024-03-26 15:14:01 -0300

    redes button

--- my-portfolio/src/components/ContactMe/Redes.tsx
+++ my-portfolio/src/components/ContactMe/Redes.tsx
@@ -16,10 +16,10 @@ const Redes = () => {
   return (
     <div className='flex flex-row justify-center items-center gap-2'>
       
-      <button onClick={() => moveToLink(gitHub)} className='bg-white w-20 h-20 hover:bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] rounded-full flex justify-center items-center'><img src={githubImg} className='w-16 h-16'/></button>
-      <button onClick={() => moveToLink(linkedIn)} className='bg-white w-20 h-20 hover:bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] rounded-full flex justify-center items-center'><img src={linkedinImg} className='w-16 h-16'/></button>
-      <button onClick={() => moveToLink(mail)} className='bg-white w-20 h-20 hover:bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] rounded-full flex justify-center items-center'><img src={emailImg} className='w-16 h-16'/></button>
-      <button onClick={() => copyText(userDiscord)} className='bg-white w-20 h-20 hover:bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] rounded-full flex justify-center items-center'><img src={discordImg} className='w-16 h-16'/></button>
+      <button onClick={() => moveToLink(gitHub)} className='w-20 h-20 bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] hover:bg-gradient-to-bl rounded-full flex justify-center items-center'><img src={githubImg} className='w-16 h-16'/></button>
+      <button onClick={() => moveToLink(linkedIn)} className='w-20 h-20 bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] hover:bg-gradient-to-bl rounded-full flex justify-center items-center'><img src={linkedinImg} className='w-16 h-16'/></button>
+      <button onClick={() => moveToLink(mail)} className='w-20 h-20 bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] hover:bg-gradient-to-bl rounded-full flex justify-center items-center'><img src={emailImg} className='w-16 h-16'/></button>
+      <button onClick={() => copyText(userDiscord)} className='w-20 h-20 bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] hover:bg-gradient-to-bl rounded-full flex justify-center items-center'><img src={discordImg} className='w-16 h-16'/></button>
         
     </div>
   )
