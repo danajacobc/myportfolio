34cba7043609e9b6290295644585dfbe6e256cad
danajacobc <danajacobc@gmail.com>
2024-03-30 17:17:34 -0300

    contact me responsive

--- my-portfolio/src/views/ContactMe/ContactMe.tsx
+++ my-portfolio/src/views/ContactMe/ContactMe.tsx
@@ -7,7 +7,7 @@ const ContactMe = () => {
     <div className="flex flex-col justify-center items-center gap-10 z-10">
       <div className='flex flex-col justify-center items-center gap-2'>
         <h2 className='font-bold text-2xl bg-gradient-to-tr from-[#E1A0E7] to-[#CD60D8] text-transparent bg-clip-text'>¡TRABAJEMOS JUNTOS!</h2>
-        <p className='font-semibold text-xl'>Si te interesa mi perfil y deseas que trabajemos en conjunto, ¡no dudes en contactarme!</p>
+        <p className='font-semibold text-xl w-96 md:w-full'>Si te interesa mi perfil y deseas que trabajemos en conjunto, ¡no dudes en contactarme!</p>
       </div>
 
       <img src={logo} className="h-56 w-56"/>
